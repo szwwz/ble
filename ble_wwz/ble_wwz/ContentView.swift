d4de32236dc3b34ac85fc781ae2ef57896e54e7f
szwwz <ccwuzhou@gmail.com>
2026-06-08 10:22:16 +0800

    1

---
+++ ble_wwz/ble_wwz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ble_wwz
+//
+//  Created by apple on 2026/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ble_wwz/ble_wwz/ble_wwzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_wwzApp.swift
+//  ble_wwz
+//
+//  Created by apple on 2026/6/8.
+//
+
+import SwiftUI
+
+@main
+struct ble_wwzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
